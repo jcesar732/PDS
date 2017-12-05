3b027db3efc5fe73aec4ccad83c8b495ccad41c2
Jonathan Cardoso <jonathan99moura@hotmail.com>
2017-12-05 13:31:15 -0300

    Update fornecedor_editar.aspx.cs

--- src/WebApplication1/fornecedor_editar.aspx.cs
+++ src/WebApplication1/fornecedor_editar.aspx.cs
@@ -13,5 +13,29 @@ namespace WebApplication1
         {
 
         }
+        protected void Button1_Click(object sender, EventArgs e)
+        {//salvar -> update
+            // Acessa a configuração da conexão
+            string s = ConfigurationManager.ConnectionStrings["PDSI_2017_Julio_TrindadeConnectionString"].ConnectionString;
+
+            // Cria Conexão com banco de dados
+            SqlConnection sqlfornecedor = new SqlConnection(s);
+
+            // Abre conexão com o banco de dados
+            sqlfornecedor.Open();
+
+            // Cria comando SQL
+            SqlCommand com = sqlfornecedor.CreateCommand();
+            
+            // define SQL do comando
+            com.CommandText = "UPDATE Fornecedor SET descricao = ' "+ TextBox1.Text + " ' WHERE codigo = " + Session["id_editar"] + "; ";
+            com.ExecuteNonQuery();
+            Response.Redirect("~/cadastro_fornecedores.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {//cancelar
+            Response.Redirect("~/cadastro_fornecedores.aspx");
+        }
     }
 }
